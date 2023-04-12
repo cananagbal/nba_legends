256cfde88a88397934ac14a9e3d3099e93680a7b
Canan Ağbal <agbalcanan8069@gmail.com>
2023-04-12 11:47:32 +0300

    handleClick

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,19 +1,25 @@
-import { Container} from "react-bootstrap"
-import {data} from '../helpers/data'
+import { Col, Container, Row } from "react-bootstrap";
+import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
-const CardContainer = ()=> {
-    return(
-        <Container className="card-container rounded-4 my-4 p-3">
-         {data.map((player)=> (
-        <PlayerCard  {...player}/>
-         ) )}
-
-        </Container>
-    )
-}
+const CardContainer = () => {
+  return (
+    <Container className="card-container rounded-4 my-4 p-3">
+      <Row className="g-3 justify-content-center">
+        {data.map((player, i) => {
+          return (
+            <Col md={6} lg={4} xl={3} key={i}>
+              <PlayerCard {...player} />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
 
 export default CardContainer;
 
-
 //data yı import ettik. data. map diyerek datanın her bir elemanını döndürdük.
+//map yapıp bir liste dönüyorsak dönün her bir childin uniq bir keyinin olması lazım id olabilir. name olabilir bunlar datada olmadığı için map bize index veriyor bunu kullanabiliriz.eğer data eksilme arttırma yapıyorsa silme yapıyorsa indexler değişeceği için datanın içindeki uniq değerleri kullanmak lazım
+//row her bir sıra ve her bir sıra içinde col yapısı olacağı için bootstrap tan row ve col yapısını import ettik.
